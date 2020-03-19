cf218ab418139a2aa54362ae0c89f304ae9f296b
Sandun Tharaka <sanduntharaka4444@gmail.com>
2020-03-19 18:33:39 +0530

    Add files via upload

---
+++ CreatePdfAndroid/Interfaces/ILocalFileProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace pdfjs.Interfaces
+{
+    public interface ILocalFileProvider
+    {
+        Task<string> SaveFileToDisk(Stream stream, string fileName);
+        //Task<string> SaveFileToDisk(Stream stream, string fileName);
+        //Task<string>  SaveFileToDisk(byte[] pdfStream, string v);
+    }
+}
