17de692d8ab56cc5f53c05d23267a308a3da16e2
Sandun Tharaka <sanduntharaka4444@gmail.com>
2020-03-19 18:47:41 +0530

    Add files via upload

---
+++ Droid/PlatformSpecifics/LocalFileProvider.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using pdfjs.Droid.PlatformSpecifics;
+using pdfjs.Interfaces;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(LocalFileProvider))]
+namespace pdfjs.Droid.PlatformSpecifics
+{
+    public class LocalFileProvider : ILocalFileProvider
+    {
+        private readonly string _rootDir = Path.Combine(Android.OS.Environment.ExternalStorageDirectory.Path, "z");
+
+        public async Task<string> SaveFileToDisk(Stream pdfStream, string fileName)
+        {
+            if (!Directory.Exists(_rootDir))
+                Directory.CreateDirectory(_rootDir);
+
+            var filePath = Path.Combine(_rootDir, fileName);
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await pdfStream.CopyToAsync(memoryStream);
+
+                //int read;
+                //while ((read = memoryStream.Read(pdfStream, 0, pdfStream.Length)) > 0)
+                //{
+                //    memoryStream.Write(pdfStream, 0, read);
+                //}
+
+                File.WriteAllBytes(filePath, memoryStream.ToArray());
+            }
+
+            return filePath;
+        }
+    }
+}
---
+++ Droid/Renderers/PdfWebViewRenderer.cs
@@ -0,0 +1,34 @@
+﻿using Android.Content;
+using pdfjs.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(WebView), typeof(PdfWebViewRenderer))]
+namespace pdfjs.Droid
+{
+	public class PdfWebViewRenderer : WebViewRenderer
+	{
+		public PdfWebViewRenderer(Context context) : base(context)
+		{
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<WebView> e)
+		{
+			base.OnElementChanged(e);
+
+			if (e.NewElement != null)
+			{
+				Control.Settings.AllowFileAccess = true;
+				Control.Settings.AllowFileAccessFromFileURLs = true;
+				Control.Settings.AllowUniversalAccessFromFileURLs = true;
+			}
+		}
+		
+		// If you want to enable scrolling in WebView uncomment the following lines.
+		//public override bool DispatchTouchEvent(MotionEvent e)
+		//{
+		//    Parent.RequestDisallowInterceptTouchEvent(true);
+		//    return base.DispatchTouchEvent(e);
+		//}
+	}
+}
